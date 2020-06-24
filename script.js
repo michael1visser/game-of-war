0feef66c7a07527384638ac8b82ae259cf694363
mike_visser <mpviss@gmail.com>
2020-06-24 14:11:27 -0400

    updating war function to include checkForWinner

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const suits = ["Hearts", "Clubs","Diamonds","Spades"]
 
-let deck = [] //Full Deck of cards
+let fullDeck = [] //Full Deck of cards
+
+//let deckSize // Total number of cards in deck
 
 let playerOne = []  //Player one's hand throughout the game
 
 let playerTwo = [] //Player two's hand throughout the game
 
+let cards1  //Cards currently in play for player 1
+
+let cards2 //Cards currently in play for player 2
+
 
 //DEFINE FUNCTION TO CREATE THE DECK OF 52 CARDS
 
-function createDeck(){
+function createDeck(deck){
 for (let i = 0; i < suits.length; i++){
     for (let j = 2; j <= 14; j++){
         if(j === 11){
@@ -30,6 +36,7 @@ for (let i = 0; i < suits.length; i++){
         
     }
 }
+ //deckSize = deck.length /2
 }
 
 
@@ -37,7 +44,7 @@ for (let i = 0; i < suits.length; i++){
 Makes use of the Fisher-Yates algorithm, taken from 
 https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb */
 
-function shuffleDeck(){
+function shuffleDeck(deck){
     for(let i = deck.length - 1; i > 0; i--){
   const j = Math.floor(Math.random() * i)
   const temp = deck[i]
@@ -49,108 +56,123 @@ function shuffleDeck(){
 
 //DEFINE FUNCTION TO DIVIDE CARDS BETWEEN THE TWO PLAYERS
 
-function divideCards(play1, play2){
+function divideCards(player1, player2, deck){
 
 //DEFINE FUNCTION TO ASSIGN HALF THE DECK TO A PLAYER
 
-    function assignCards(deckArray, player){
-        let deckLength = deckArray.length / 2
+    function assignCards(deck, player){
+        //console.log(deckSize)
+
         for (let i=0; i < 26; i++){
-            player.push(deckArray[0])
-            deckArray.shift()
+            player.push(deck[0])
+            deck.shift()
         }
     }
 
 //ASSIGN CARDS TO EACH PLAYER
 
-assignCards(deck, playerOne)
-assignCards(deck, playerTwo)
+assignCards(deck, player1)
+assignCards(deck, player2)
 
 }
 
 //DEFINE FUNCTION TO CHECK FOR A WINNER
 
-function checkForWinner(player1, player2){
-if (player1.length >= 35){
+function checkForWinner(player1, player2, winTotal, runRound){
+if (player1.length >= winTotal){
     console.log("Game over! Player 1 wins!")
 }
-else if (player2.length >= 35){
+else if (player2.length >= winTotal){
     console.log("Game over! Player 2 wins!")
 }
 else {
-    playRound(player1, player2)
+    runRound(player1, player2)
 }
 }
 
 //DEFINE FUNCTION TO COMPARE CARDS
 
-function compareCard(card1, originDeck1, card2, originDeck2, position){
+function compareCard(card1, player1, card2, player2, position){
     if (card1[position].value > card2[position].value){
         for (let i=0; i < card1.length; i++){
-            originDeck1.push(card1[i])
+            player1.push(card1[i])
                 }
          for (let j=0; j < card2.length; j++){
-             originDeck1.push(card2[j])
+            player1.push(card2[j])
              }
         console.log(`Player 1 plays ${card1[position].name}, Player 2 plays ${card2[position].name}. Player 1 wins!`)
 
-        console.log(`player 1 has ${playerOne.length} cards. p2  has ${playerTwo.length} cards`)
+        console.log(`player 1 has ${player1.length} cards. p2  has ${player2.length} cards`)
 
-        checkForWinner(originDeck1, originDeck2)
+        checkForWinner(player1, player2, 52, playRound)
 
     }
     else if (card1[position].value < card2[position].value){
         for (let i=0; i < card1.length; i++){
-        originDeck2.push(card1[i])
+            player2.push(card1[i])
             }
         for (let j=0; j < card2.length; j++){
-            originDeck2.push(card2[j])
+            player2.push(card2[j])
             }
         console.log(`Player 1 plays ${card1[position].name}, Player 2 plays ${card2[position].name}. Player 2 wins!`)
         
-        console.log(`player 1 has ${playerOne.length} cards. p2  has ${playerTwo.length} cards`)
+        console.log(`player 1 has ${player1.length} cards. p2  has ${player2.length} cards`)
 
-        checkForWinner(originDeck1, originDeck2)
+        checkForWinner(player1, player2, 52)
     }
     else {
         console.log(`Player 1 plays ${card1[position].name}, Player 2 plays ${card2[position].name}. WAAAAAAAR!`)
         
-        war(card1, originDeck1, card2, originDeck2)
+        checkForWinner(player1, player2, 49, war)
+        //war(card1, player1, card2, player2)
     }
 }
 
 //DEFINE FUNCTION FOR WAR
-function war(initialCard1, playerOne, initialCard2, playerTwo){
-    let p1War = initialCard1.concat(playerOne.splice(0,4))
+function war(player1, player2){
+
+
+    cards1 = cards1.concat(player1.splice(0,4))
        // console.log(p1War)
-    let p2War = initialCard2.concat(playerTwo.splice(0,4))
+    cards2 = cards2.concat(player2.splice(0,4))
                                                                                          //console.log(p2War)
 
-    let warPosition = Math.min(p1War.length, p2War.length) - 1
-        //console.log(warPosition)
-    compareCard(p1War, playerOne, p2War, playerTwo, warPosition)
+    compareCard(cards1, player1, cards2, player2, 4)
 }
 
 //DEFINE FUNCTION TO PLAY A ROUND. RECURRING IF THERE IS WAR
 
-function playRound(cardsInPlay1, cardsInPlay2){
+function playRound(player1, player2){
 
-    let p1Cards = cardsInPlay1.splice(0,1)
+    let cards1 = player1.splice(0,1)
 
-    let p2Cards = cardsInPlay2.splice(0,1)
+    let cards2 = player2.splice(0,1)
     
-compareCard(p1Cards, playerOne, p2Cards, playerTwo, 0)
+compareCard(cards1, player1, cards2, player2, 0)
 
 }
 
    
 
 function playWar(){
-createDeck()
+createDeck(fullDeck)
 
-shuffleDeck()
+shuffleDeck(fullDeck)
 
-divideCards(playerOne, playerTwo)
+divideCards(playerOne, playerTwo, fullDeck)
 
-checkForWinner(playerOne, playerTwo)
+checkForWinner(playerOne, playerTwo, 52, playRound)
+}
+
+
+//USE SPREAD OPERATOR (...) ON NEW ARRAYS TO MAKE SURE ORIGINAL ARRAYS ARE NOT AFFECTED BY POP/PUSH/ETC... 
+//CAN I REDUCE DUPLICATE CODE WITH FUNCTION REFERENCES?
+
+
+function testDeck(){
+    createDeck(fullDeck)
+
+shuffleDeck(fullDeck)
+
+divideCards(playerOne, playerTwo, fullDeck)
 }
